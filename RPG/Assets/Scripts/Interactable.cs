fc990a1885b356f7076644cf8570be93118208fd
Aedox <mgavin618@yahoo.com>
2020-04-07 19:08:50 -0500

    First Commit
    First Commit

---
+++ RPG/Assets/Scripts/Interactable.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float radius = 3f;
+    public Transform interactionTransform;
+
+    bool isFocus = false;
+    Transform player;
+
+    bool hasInteracted = false;
+
+    public virtual void Interact()
+    {
+        // This method is meant to be overwritten
+        Debug.Log("Interact" + transform.name);
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(interactionTransform.position, radius);
+    }
+
+    private void Update()
+    {
+        if (isFocus && !hasInteracted)
+        {
+            float distance = Vector3.Distance(player.position, interactionTransform.position);
+            if(distance <= radius)
+            {
+                Interact();
+                hasInteracted = true;
+
+            }
+        }
+    }
+    public void OnFocused(Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+        hasInteracted = false;
+    }
+
+    public void OnDefocused()
+    {
+        isFocus = false;
+        player = null;
+        hasInteracted = false;
+    }
+}
